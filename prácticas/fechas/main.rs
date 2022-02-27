24730b63ac0f487d82aafb9a2efbcf0b96979fed
Rafael Aybar <rafaelaybar@hotmail.com>
2022-02-27 16:32:13 +0100

    Pruebas con fechas

---
+++ prácticas/fechas/main.rs
@@ -0,0 +1,9 @@
+// Fecha actual  en distintos formatos
+// Operaciones de suma y resta de fechas
+
+use std::time::SystemTime;
+
+fn main() {
+    println!("Pruebas con fechas");
+    println!("La fecha actual es: {:?}", SystemTime::now() )
+}
