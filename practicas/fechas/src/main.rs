1e13c72a887cf461bceef539988c260bf3233526
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2022-03-04 16:54:24 +0100

    Ahora cada ejemplo es un proyecto diferente

---
+++ practicas/fechas/src/main.rs
@@ -0,0 +1,8 @@
+/*Fecha actual  en distintos formatos, operaciones de suma y resta de fechas */
+
+use std::time::SystemTime;
+
+fn main() {
+    println!("Pruebas con fechas");
+    println!("La fecha actual en formato UNIX es: {:?}", SystemTime::now() )
+}
--- prácticas/fechas/main.rs
+++
@@ -1,9 +0,0 @@
-// Fecha actual  en distintos formatos
-// Operaciones de suma y resta de fechas
-
-use std::time::SystemTime;
-
-fn main() {
-    println!("Pruebas con fechas");
-    println!("La fecha actual es: {:?}", SystemTime::now() )
-}
